7132da38f7190bd766d69a2807fd43eb25404111
lucio2001-dotcom <71552113+lucio2001-dotcom@users.noreply.github.com>
2020-11-08 12:45:29 +0100

    Add files via upload

--- calcolatrice.c
+++ calcolatrice.c
@@ -1,55 +1,55 @@
 #include <stdio.h>
 int main ()
 {
-    int var1;
-    int var2;
-    int var3;
-    int var4;
-    int num1;
-    int num2;
+    float var1;
+    float var2;
+    float var3;
+    float var4;
+    float num1;
+    float num2;
     int ch;
-printf("calcolatrice\n premi 1 per addizione\n premi 2 per sottrazione\n premi 3 per moltiplicazione\n premi 4 per divisione");
+printf("calcolatrice\n premi 1 per addizione\n premi 2 per sottrazione\n premi 3 per moltiplicazione\n premi 4 per divisione\n");
     while ((ch = getchar())  !=EOF)
     {
         switch (ch)
         {
         case '1':
            { printf ("inserisci il primo numero  ");
-             scanf ("%d",&num1);
+             scanf ("%f",&num1);
              printf ("inserisci il secondo numero  ");
-             scanf ("%d",&num2);
+             scanf ("%f",&num2);
              var1=num1+num2;
-             printf ("la somma è:   %d",var1);
+             printf ("la somma è:   %f",var1);
            }
             break;
         case '2':
             {
                 printf ("inserisci il primo numero  ");
-                scanf  ("%d",&num1);
+                scanf  ("%f",&num1);
                 printf ("inserisci il secondo numero  ");
-                scanf  ("%d",&num2);
+                scanf  ("%f",&num2);
                 var2=num1-num2;
-                printf ("la differenza è:   %d",var2);
+                printf ("la differenza è:   %f",var2);
             }
             break;
         case '3':
             {
                 printf ("inserisci il primo numero  ");
-                scanf  ("%d",&num1);
+                scanf  ("%f",&num1);
                 printf ("inserisci il secondo numero  ");
-                scanf  ("%d",&num2);
+                scanf  ("%f",&num2);
                 var3=num1*num2;
-                printf ("il prodotto è:   %d",var3);
+                printf ("il prodotto è:   %f",var3);
             }
             break;
         case '4':
             {
                 printf ("inserisci il primo numero  ");
-                scanf  ("%d",&num1);
+                scanf  ("%f",&num1);
                 printf ("inserisci il secondo numero  ");
-                scanf  ("%d",&num2);
+                scanf  ("%f",&num2);
                 var4=num1/num2;
-                printf ("il risultato è:   %d",var4);
+                printf ("il risultato è:   %f",var4);
 
 
             }
